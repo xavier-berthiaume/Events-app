b0b2b606a9881a70497d464e1f3484064e230bc2
Xavier Berthiaume <xavier.berthiaume@gmail.com>
2023-07-26 22:29:26 -0400

    Added event models

--- clubs/events/admin.py
+++ clubs/events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Venue, ClubUser, Event
 
 # Register your models here.
+admin.site.register(Venue)
+admin.site.register(ClubUser)
+admin.site.register(Event)
