07346c5d14e75556c1ee8d55ff77ae0f55c76724
Xavier Berthiaume <xavier.berthiaume@gmail.com>
2023-07-29 21:33:53 -0400

    Added user login feature

--- clubs/members/views.py
+++ clubs/members/views.py
@@ -7,14 +7,17 @@ def login_user(request):
     context = {}
 
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username = username, password = password)
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        user = authenticate(request=request, username = username, password = password)
 
         if user is not None:
             login(request, user)
+            return redirect('home')
+
         else:
-            pass
+            messages.success(request, "There was an error logging in, try again")
+            return redirect('login-user')
 
     else:
         return render(request, 'authentication/login.html', context)
